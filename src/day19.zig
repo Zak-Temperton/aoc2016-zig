381ad9369a805ed4f435bdb2d21cbf107b2719b7
Zak-Temperton <zbtemperton@gmail.com>
2023-10-15 14:00:58 +1300

    day19

--- src/day19.zig
+++ src/day19.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
 const List = std.ArrayList;
+const LinkedList = std.TailQueue;
 const Map = std.AutoHashMap;
 const StrMap = std.StringHashMap;
 const BitSet = std.DynamicBitSet;
@@ -10,7 +11,27 @@ const gpa = util.gpa;
 
 const data = @embedFile("data/day19.txt");
 
-pub fn main() !void {}
+fn part1(num: u32) u32 {
+    var i: u5 = 1;
+    const two: u32 = 2;
+    while (two << (i + 1) <= num) i += 1;
+    return 2 * (num - (two << i)) + 1;
+}
+fn part2(num: u32) u32 {
+    var i: u32 = 1;
+    while (i * 3 < num) i *= 3;
+    return num - i;
+}
+pub fn main() !void {
+    const num = 3004953;
+    var timer = try std.time.Timer.start();
+    const p1 = part1(num);
+    const p1_time = timer.read();
+    const p2 = part2(num);
+    const p2_time = timer.read();
+    print("{d} {d}ns\n", .{ p1, p1_time });
+    print("{d} {d}ns\n", .{ p2, p2_time });
+}
 
 // Useful stdlib functions
 const tokenize = std.mem.tokenize;
