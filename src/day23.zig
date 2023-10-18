a0ed1556841542b6891f621d8b52551b8396d5b9
Zak-Temperton <zbtemperton@gmail.com>
2023-10-19 11:20:12 +1300

    day23

--- src/day23.zig
+++ src/day23.zig
@@ -10,7 +10,169 @@ const gpa = util.gpa;
 
 const data = @embedFile("data/day23.txt");
 
-pub fn main() !void {}
+const Register = union(enum) {
+    const Self = @This();
+
+    val: i64,
+    reg: u8,
+
+    fn from(r: []const u8) !Self {
+        switch (r[0]) {
+            'a'...'z' => |c| {
+                return .{ .reg = c - 'a' };
+            },
+            else => {
+                return .{ .val = try parseInt(i64, r, 10) };
+            },
+        }
+    }
+};
+
+const Instruction = union(enum) {
+    const two_args = struct { a: Register, b: Register };
+    const one_arg = struct { a: Register };
+    const Self = @This();
+
+    cpy: two_args,
+    jnz: two_args,
+    tgl: one_arg,
+    inc: one_arg,
+    dec: one_arg,
+
+    fn toggle(self: *Self) void {
+        self.* = switch (self.*) {
+            .cpy => |s| .{ .jnz = .{ .a = s.a, .b = s.b } },
+            .jnz => |s| .{ .cpy = .{ .a = s.a, .b = s.b } },
+            .tgl => |s| .{ .inc = .{ .a = s.a } },
+            .dec => |s| .{ .inc = .{ .a = s.a } },
+            .inc => |s| .{ .dec = .{ .a = s.a } },
+        };
+    }
+};
+
+fn parseLine(input: []const u8) ?Instruction {
+    var words = tokenize(u8, input, " ");
+    var result: ?Instruction = null;
+    switch (words.next().?[0]) {
+        'c' => {
+            const a = Register.from(words.next().?) catch return null;
+            const b = Register.from(words.next().?) catch return null;
+            result = .{ .cpy = .{ .a = a, .b = b } };
+        },
+        'j' => {
+            const a = Register.from(words.next().?) catch return null;
+            const b = Register.from(words.next().?) catch return null;
+            result = .{ .jnz = .{ .a = a, .b = b } };
+        },
+        't' => {
+            const a = Register.from(words.next().?) catch return null;
+            result = .{ .tgl = .{ .a = a } };
+        },
+        'd' => {
+            const a = Register.from(words.next().?) catch return null;
+            result = .{ .dec = .{ .a = a } };
+        },
+        'i' => {
+            const a = Register.from(words.next().?) catch return null;
+            result = .{ .inc = .{ .a = a } };
+        },
+        else => {
+            return null;
+        },
+    }
+    return result;
+}
+
+fn solve(input: i64) !i64 {
+    var registers: [4]i64 = .{ input, 0, 0, 0 };
+    var instructions = List(Instruction).init(gpa);
+    var lines = tokenize(u8, data, "\r\n");
+    while (lines.next()) |line| {
+        try instructions.append(parseLine(line).?);
+    }
+    var index: usize = 0;
+    while (index < instructions.items.len) {
+        switch (instructions.items[index]) {
+            .cpy => |instr| {
+                const in = switch (instr.a) {
+                    .reg => |r| registers[r],
+                    .val => |v| v,
+                };
+                switch (instr.b) {
+                    .reg => |r| registers[r] = in,
+                    .val => {},
+                }
+                index += 1;
+            },
+            .jnz => |instr| {
+                const in = switch (instr.a) {
+                    .reg => |r| registers[r],
+                    .val => |v| v,
+                };
+                if (in != 0) {
+                    switch (instr.b) {
+                        .reg => |r| {
+                            if (registers[r] < 0) {
+                                index -= @intCast(-registers[r]);
+                            } else {
+                                index += @intCast(registers[r]);
+                            }
+                        },
+                        .val => |v| {
+                            if (v < 0) {
+                                index -= @intCast(-v);
+                            } else {
+                                index += @intCast(v);
+                            }
+                        },
+                    }
+                } else {
+                    index += 1;
+                }
+            },
+            .tgl => |instr| {
+                const in = switch (instr.a) {
+                    .reg => |r| registers[r],
+                    .val => |v| v,
+                };
+                var target = index;
+                if (in < 0) {
+                    target -= @intCast(-in);
+                } else {
+                    target += @intCast(in);
+                }
+                if (target < instructions.items.len) instructions.items[target].toggle();
+                index += 1;
+            },
+            .dec => |instr| {
+                switch (instr.a) {
+                    .reg => |r| registers[r] -= 1,
+                    .val => {},
+                }
+
+                index += 1;
+            },
+            .inc => |instr| {
+                switch (instr.a) {
+                    .reg => |r| registers[r] += 1,
+                    .val => {},
+                }
+                index += 1;
+            },
+        }
+    }
+    return registers[0];
+}
+pub fn main() !void {
+    var timer = try std.time.Timer.start();
+    const p1 = try solve(7);
+    const p1_time = timer.read();
+    print("{d} {d}ns\n", .{ p1, p1_time });
+    timer.reset();
+    const p2 = try solve(12);
+    const p2_time = timer.read();
+    print("{d} {d}ns\n", .{ p2, p2_time });
+}
 
 // Useful stdlib functions
 const tokenize = std.mem.tokenize;
